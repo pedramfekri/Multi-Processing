c1041417bbe7f7b7c63ac38476ff7c1abf91bda6
Pedram <pedram.fekri@gmail.com>
2022-01-03 15:14:15 -0500

    c

--- main.py
+++ main.py
@@ -1,15 +1,26 @@
 import time
 import multiprocessing
+import numpy as np
+
+manager = multiprocessing.Manager()
+final_list1 = manager.list()
+final_list2 = manager.list()
 
 
 def fun1(): # for example your potentiometer
-    for i in range(5):
-        time.sleep(1)
+    c = 10
+    for i in range(20):
+        final_list1.append(c)
+        c = c + 1
+        # time.sleep(1)
 
 
 def fun2(): # for example your force sensor
-    for i in range(5):
-        time.sleep(1)
+    c = -10
+    for i in range(20):
+        final_list2.append(c)
+        c = c + 1
+        # time.sleep(1)
 
 
 def normal_run():
@@ -30,6 +41,13 @@ def multi_process_run():
     process2.join()
     toc = time.time()
     print('total time = ', toc - tic)
+    r = np.zeros([20, 2])
+    f1 = np.array(final_list1)
+    f2 = np.array(final_list2)
+    # print(f1)
+    r[:, 0] = f1
+    r[:, 1] = f2
+    print(r)
 
 
 if __name__ == '__main__': # alternate to see the effect
